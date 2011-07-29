04435c9e9425ed84c8a3ef62fe99fa3f0ceb473e
Adolfo Neto <adolfo.usp@gmail.com>
2011-07-29 10:16:41 -0300

    All analytic tableaux rules implemented

--- Clojure/experiments/formula.clj
+++ Clojure/experiments/formula.clj
@@ -1,4 +1,6 @@
+;; Tests for an implementation of Smullyan's Signed Analytic Tableaux for Classical Proposistional Logic
 ;; Adolfo Neto, 2011
+
 ;; A signed formula will be represented as
 	;; [SIGN [CONECTIVE subformulas]]
 ;; I am not representing atomic formulas yet...
@@ -16,7 +18,8 @@
 
 (defn OnePremiseTwoConclusionsSignedRule
   [premiseSign premiseConnective leftConclusionSign rightConclusionSign]
-  (fn [sign [connective left right]] (if (and
+  (fn [sign [connective left right]] 
+    (if (and
        (= sign premiseSign)
        (= connective premiseConnective)
        )
@@ -28,18 +31,81 @@
  )
 )
 
+(defn OnePremiseOneConclusionSignedRule
+  [premiseSign premiseConnective conclusionSign]
+  (fn [sign [connective formula]] 
+   (if (and
+       (= sign premiseSign)
+       (= connective premiseConnective)
+       )
+    (vector
+     (vector conclusionSign formula)
+     )
+    'nil)
+ )
+)
+
+(defn OnePremiseTwoBranchedConclusionsSignedRule
+  [premiseSign premiseConnective leftConclusionSign rightConclusionSign]
+  (fn [sign [connective left right]] 
+    (if (and
+       (= sign premiseSign)
+       (= connective premiseConnective)
+       )
+    (list 
+     (vector (vector leftConclusionSign left))
+     (vector (vector rightConclusionSign right))
+     )
+    'nil)
+ )
+)
+
 (defn TAnd
   "Applies the T-AND rule to the formula.
-   Example: T A&B ==> T A, T B
-   "
+   Example: T A&B ==> T A, T B"
    [signedFormula] 
    (apply (OnePremiseTwoConclusionsSignedRule 'T '& 'T 'T)  signedFormula))
 
 
 (defn FOr 
   "Applies the F-OR rule to the formula.
-   Example: F A|B ==> F A, F B
-   "
+   Example: F A|B ==> F A, F B"
   [signedFormula] 
   (apply (OnePremiseTwoConclusionsSignedRule 'F '| 'F 'F)  signedFormula))
+
+(defn FImplies
+  "Applies the F-Implies rule to the formula.
+   Example: F A->B ==> T A, F B"
+  [signedFormula] 
+  (apply (OnePremiseTwoConclusionsSignedRule 'F '-> 'T 'F)  signedFormula))
+
+(defn TNot
+  "Applies the T-Not rule to the formula.
+   Example: T !A ==> F A"
+  [signedFormula] 
+  (apply (OnePremiseOneConclusionSignedRule 'T '! 'F)  signedFormula))
+
+(defn FNot
+  "Applies the F-Not rule to the formula.
+   Example: F !A ==> T A"
+  [signedFormula] 
+  (apply (OnePremiseOneConclusionSignedRule 'F '! 'T)  signedFormula))
+
+(defn FAnd
+  "Applies the F-And rule to the formula.
+   Example: [F A&B] ==> ( [F A]  [F B] )"
+  [signedFormula] 
+  (apply (OnePremiseTwoBranchedConclusionsSignedRule 'F '& 'F 'F)  signedFormula))
+
+(defn TOr
+  "Applies the T-Or rule to the formula.
+   Example: [T A|B] ==> ( [T A]  [T B] )"
+  [signedFormula] 
+  (apply (OnePremiseTwoBranchedConclusionsSignedRule 'T '| 'T 'T)  signedFormula))
+
+(defn TImplies
+  "Applies the T-Implies rule to the formula.
+   Example: [T A->B] ==> ( [F A]  [T B] )"
+  [signedFormula] 
+  (apply (OnePremiseTwoBranchedConclusionsSignedRule 'T '-> 'F 'T)  signedFormula))
 
--- Clojure/experiments/tdd.clj
+++ Clojure/experiments/tdd.clj
@@ -1,3 +1,4 @@
+;; Tests for an implementation of Smullyan's Signed Analytic Tableaux for Classical Proposistional Logic
 ;; Adolfo Neto, 2011
 ;; 03/02/2011, 29/07/2011
 
@@ -8,6 +9,12 @@
 (def t-and-example-1 '[T [& A B]])
 (def t-and-example-2 '[T [& C D]])
 (def f-or-example-1 '[F [| A B]])
+(def f-implies-example-1 '[F [-> A B]])
+(def t-not-example '[T [! A]])
+(def f-not-example '[F [! A]])
+(def f-and-example '[F [& A B]])
+(def t-or-example '[T [| A B]])
+(def t-implies-example '[T [-> A B]])
 
 (deftest getsign-test
   (is (= 'T (getsign t-and-example-1)))
@@ -17,6 +24,7 @@
   (is (= '& (getconnective t-and-example-1)))
   )
 
+;; one premise two conclusions rules
 (deftest TAnd-test
   (is (= '[[T A] [T B]] (TAnd t-and-example-1)))
   (is (= '[[T C] [T D]] (TAnd t-and-example-2)))
@@ -25,6 +33,36 @@
 (deftest FOr-test
   (is (= '[[F A] [F B]] (FOr f-or-example-1)))
   )
+
+
+(deftest FImplies-test
+  (is (= '[[T A] [F B]] (FImplies f-implies-example-1)))
+  )
+
+;; one premise one conclusion rules
+(deftest TNot-test
+  (is (= '[[F A]] (TNot t-not-example)))
+  )
+
+(deftest FNot-test
+  (is (= '[[T A]] (FNot f-not-example)))
+  )
+
+;; one premise two branches rules
+(deftest FAnd-test
+  (is (= '([[F A]] [[F B]]) (FAnd f-and-example)))
+  )
+
+(deftest TOr-test
+  (is (= '([[T A]] [[T B]]) (TOr t-or-example)))
+  )
+
+(deftest TImplies-test
+  (is (= '([[F A]] [[T B]]) (TImplies t-implies-example)))
+  )
+
+
+
 
 ;;(deftest getformula-test
 ;;  (is (= '[& A B] (getformula t-and-example)))) 
