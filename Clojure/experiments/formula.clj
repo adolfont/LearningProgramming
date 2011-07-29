4043e7723836e48e8dfeaab8199bab10ab493e89
Adolfo Neto <adolfo.usp@gmail.com>
2011-07-29 09:31:49 -0300

    Added a function for one premise two conclusion rules

--- Clojure/experiments/formula.clj
+++ Clojure/experiments/formula.clj
@@ -1,8 +1,18 @@
 ;; Adolfo Neto, 2011
 ;; A signed formula will be represented as
-	;; (SIGN (CONECTIVE formula))
+	;; [SIGN [CONECTIVE subformulas]]
 ;; I am not representing atomic formulas yet...
 
+
+(defn getsign [formula]
+  "gets the sign of a signed formula"
+  (first formula)
+)
+
+(defn getconnective [formula]
+  "gets the main connective of the signed formula"
+  (first (second formula))
+)
 
 (defn TAnd
   "Applies the T-AND rule to the formula.
@@ -19,3 +29,38 @@
      )
     'nil)
   )
+
+(comment
+(defn FOr
+  "Applies the F-OR rule to the formula.
+   Example: F A|B ==> F A, F B
+   "
+  [[sign [connective left right]]]
+  (if (and
+       (= sign 'F)
+       (= connective '|)
+       )
+    (vector
+     (vector 'F left)
+     (vector 'F right)
+     )
+    'nil)
+  )
+)
+
+(defn OnePremiseTwoConclusionsSignedRule
+  [premiseSign premiseConnective leftConclusionSign rightConclusionSign]
+  (fn [sign [connective left right]] (if (and
+       (= sign premiseSign)
+       (= connective premiseConnective)
+       )
+    (vector
+     (vector leftConclusionSign left)
+     (vector rightConclusionSign right)
+     )
+    'nil)
+ )
+)
+
+(defn FOr [signedFormula] (apply (OnePremiseTwoConclusionsSignedRule 'F '| 'F 'F)  signedFormula))
+
--- Clojure/experiments/tdd.clj
+++ Clojure/experiments/tdd.clj
@@ -1,24 +1,29 @@
 ;; Adolfo Neto, 2011
-;; 03/02/2011
+;; 03/02/2011, 29/07/2011
 
 (use 'clojure.test)
 
 (load-file "formula.clj")
 
-(def t-and-example '[T [& A B]])
+(def t-and-example-1 '[T [& A B]])
 (def t-and-example-2 '[T [& C D]])
+(def f-or-example-1 '[F [| A B]])
 
-;;(deftest getsign-test
-;;  (is (= 'T (getsign t-and-example)))
-;;  )
+(deftest getsign-test
+  (is (= 'T (getsign t-and-example-1)))
+  )
 
-;;(deftest getconnective-test
-;;  (is (= '& (getconnective t-and-example)))
-;;  )
+(deftest getconnective-test
+  (is (= '& (getconnective t-and-example-1)))
+  )
 
-(deftest tand-test
-  (is (= '[[T A] [T B]] (TAnd t-and-example)))
+(deftest TAnd-test
+  (is (= '[[T A] [T B]] (TAnd t-and-example-1)))
   (is (= '[[T C] [T D]] (TAnd t-and-example-2)))
+  )
+
+(deftest FOr-test
+  (is (= '[[F A] [F B]] (FOr f-or-example-1)))
   )
 
 ;;(deftest getformula-test
