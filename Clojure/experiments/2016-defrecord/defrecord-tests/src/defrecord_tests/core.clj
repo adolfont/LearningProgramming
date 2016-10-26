be5d3cb080a72ff6be0ed3e4b738c1cc1d9a3825
Adolfo Gustavo Serra Seca Neto <adolfo.usp@gmail.com>
2016-10-26 15:43:29 -0200

    defrecord: passing one clause test and one literal test

--- Clojure/experiments/2016-defrecord/defrecord-tests/src/defrecord_tests/core.clj
+++ Clojure/experiments/2016-defrecord/defrecord-tests/src/defrecord_tests/core.clj
@@ -8,12 +8,10 @@
 
 (defrecord Literal [value])
 
-(def literal-1 (Literal. -1))
-(def literal-2 (Literal. +2))
-
 (defn literal-value [aLiteral]
   (:value aLiteral))
 
-(defrecord Clause [literal-list])
+(defrecord Clause [literal-set])
 
-(def aClause (Clause. [literal-1 literal-2]))
+(defn clause-value [aClause]
+  (:literal-set aClause))
--- Clojure/experiments/2016-defrecord/defrecord-tests/test/defrecord_tests/core_test.clj
+++ Clojure/experiments/2016-defrecord/defrecord-tests/test/defrecord_tests/core_test.clj
@@ -1,8 +1,16 @@
 (ns defrecord-tests.core-test
   (:require [clojure.test :refer :all]
-            [defrecord-tests.core :refer (->Literal literal-value)]))
+            [defrecord-tests.core :refer (->Literal literal-value
+                                      ->Clause clause-value)]))
 
-(deftest aTest
+(deftest literal-test
   (def aLiteral (->Literal -1))
   (is (= -1 (literal-value aLiteral)))
+)
+
+(deftest clause-test
+  (def aLiteral1 (->Literal -1))
+  (def aLiteral2 (->Literal +2))
+  (def aClause (->Clause #{aLiteral1 aLiteral2}))
+  (is (= #{aLiteral1 aLiteral2} (clause-value aClause)))
 )
